3829cac256a5617ce7081a62b5e5e6fb33b2c70b
Alex Lapinski <contact@alexlapinski.name>
2018-06-03 22:05:22 -0400

    Added ability to fetch entire list of receipts (date, amount, url) from receipts page using eval

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
+const _ = require('lodash');
 const puppeteer = require('puppeteer');
 const fs = require('fs');
 const path = require('path');
+const moment = require('moment');
+const Promise = require('bluebird');
 const actions = require('./actions');
 const config = require('./config');
 
+const log = console.log;
 
 const main = async () => {
 
@@ -11,6 +15,7 @@ const main = async () => {
     const page = await browser.newPage();
     await page.setViewport({width: config.viewport.width, height: config.viewport.height});
 
+    log('Signing In');
     await actions.signIn(page);
 
     const screenshotDir = path.resolve(process.cwd(), 'screenshots');
@@ -20,9 +25,30 @@ const main = async () => {
 
     await page.screenshot({path: path.resolve(screenshotDir, 'signin.png')});
 
+    log('Navigating to Receipts Page');
     await page.goto(`${config.baseUrl}/my-receipts.html`);
 
     await page.screenshot({path: path.resolve(screenshotDir, 'receipts.png')});
+
+
+    // Get table of receipt totals / date
+    const receipts = await page.$$eval('.recall-table-set', (rowParts) =>
+        Array.from(rowParts).map(row => {
+            const dateElem = row.querySelector('.date-time');
+            const valueElem = row.querySelector('.sold-col');
+            const urlElem = row.querySelector('.view-col a');
+
+            return {
+                dateTime: dateElem ? dateElem.innerText : null,
+                value: valueElem ? valueElem.innerText: null,
+                url: urlElem ? urlElem.href : null,
+            };
+        })
+    );
+
+    console.log(JSON.stringify(receipts));
+
+    await page.waitFor(240 * 1000);
 
     await browser.close();
 };
