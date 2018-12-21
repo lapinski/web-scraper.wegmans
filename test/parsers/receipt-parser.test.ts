afe87927d102b767a1cdc759c00a07cf9d82a1ef
Alex Lapinski <contact@alexlapinski.name>
2018-12-21 11:57:22 -0500

    Added test cases for 'maybe'

--- test/parsers/receipt-parser.test.ts
+++ test/parsers/receipt-parser.test.ts
@@ -32,7 +32,25 @@ describe('receipt parser', () => {
   });
 
   describe('maybe', () => {
+    const identity = (i:string) => i
+    const inverse = (i:string):string|undefined => undefined;
 
+    const testCases:{in:string, out: string|null, f:(i:string)=>string|undefined}[] = [
+      {in: null, out: undefined, f: identity},
+      {in: 'value', out: 'value', f: identity},
+      {in: 'value', out: undefined, f: inverse},
+      {in: null, out: undefined, f: inverse},
+    ];
+
+    for(let i = 0, len = testCases.length; i < len; i++) {
+      const testCase = testCases[i];
+      describe(`when given the input value '${testCase.in}' and '${testCase.f}' as the test function`, () => {
+        it(`should return '${testCase.out}'`, () => {
+          const output = receiptParser.maybe(testCase.f, testCase.in);
+          expect(output).to.equal(testCase.out);
+        });
+      });
+    }
   });
 
   describe('parseDate', () => {
