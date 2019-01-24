3382346cb678e3c68db895adf9e67cd6aee55e28
Alex Lapinski <contact@alexlapinski.name>
2019-01-23 21:56:04 -0500

    Added numerous tests for element-helpers

--- src/actions/element-helpers.ts
+++ src/actions/element-helpers.ts
@@ -1,9 +1,12 @@
-import { URL } from "url";
+import { URL } from 'url';
 import * as _ from 'lodash';
 import moment, { Moment } from 'moment';
-import { maybe } from '../parsers/receipt-parser';
 
-function extractTextContent(element: Element): string {
+const maybe = (test: (input: string) => string|undefined, value: string) =>
+    (test(value) ? value : undefined);
+
+
+export function extractTextContent(element: Element): string {
     if (!element || !element.textContent) {
         return undefined;
     }
@@ -11,37 +14,32 @@ function extractTextContent(element: Element): string {
     return element.textContent.toString();
 }
 
-function extractAnchorUrl(element: Element): URL {
+export function extractAnchorUrl(element: Element): URL {
     if (!element || !element.hasAttribute('href')) {
         return undefined;
     }
 
-    return new URL(element.getAttribute('href'));
+    try {
+        return new URL(element.getAttribute('href'));
+    } catch(error) {
+        return undefined;
+    }
 }
 
-function removeNewline(input: string):string {
+export function removeNewline(input: string):string {
     return _.replace(input, /\r?\n|\r/g, '');
 }
 
-const parseDate = _.partial(moment, _, 'MMM. DD, YYYY hh:mma');
+export const parseDate = _.partial(moment, _, 'MMM. DD, YYYY hh:mma');
 
-const sanitizeDate: (input: string) => Moment = _.flow(
-    _.partial(maybe, _.isString),
-    removeNewline, // TOOD: Move to input
+export const sanitizeDate: (input: string) => Moment = _.flow(
+    _.partial(maybe, _.isString), // TODO: Move to input
+    removeNewline, // TODO: Move to input
     parseDate, // TODO: Move to input
 );
 
-const sanitizeNumber: (input: string) => number = _.flow(
+export const sanitizeNumber: (input: string) => number = _.flow(
     _.partial(maybe, _.isString),
     removeNewline,
     _.toNumber,
 );
-
-
-export {
-    sanitizeDate,
-    sanitizeNumber,
-    extractAnchorUrl,
-    extractTextContent,
-    removeNewline,
-}
