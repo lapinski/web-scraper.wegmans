12ce71010d4f88b21cd2e8c4133d48efb562035a
Alex Lapinski <contact@alexlapinski.name>
2019-04-14 11:28:50 -0400

    WIP Extracting ReceiptDetails (Transactions)

--- src/actions/get-receipt-transactions.ts
+++ src/actions/get-receipt-transactions.ts
@@ -1,4 +1,4 @@
-import { Maybe, Nothing } from 'purify-ts/adts/Maybe';
+import { Just, Maybe, Nothing } from 'purify-ts/adts/Maybe';
 import { Page } from 'puppeteer';
 import R from 'ramda';
 import * as url from 'url';
@@ -8,10 +8,36 @@ import { ReceiptSummary } from './get-receipt-summary-list';
 import { navigateToUrlAndWait } from './browser-helpers';
 import { ReceiptDetailPageObjectModel } from '../page-objects/receipt-detail.page';
 import { ActionResponse } from './types';
+import { Moment } from 'moment';
 
+export interface Transaction {
+
+}
+
+export interface RawReceipt {
+    date: Maybe<Moment>;
+    receiptInfo: {
+        place: Maybe<string>;
+        lane: Maybe<string>;
+        operator: Maybe<string>;
+    };
+    transactions: Maybe<Transaction[]>;
+    totalSavings: Maybe<number>;
+    totalTax: Maybe<number>;
+    totalAmount: Maybe<number>;
+}
 
 export interface Receipt {
-
+    date: Moment;
+    receiptInfo: {
+        place: string;
+        lane: string;
+        operator: string;
+    };
+    transactions: Transaction[];
+    totalSavings: number;
+    totalTax: number;
+    totalAmount: number;
 }
 
 const extractPlace = (selector: string, context: Cheerio) =>
@@ -34,26 +60,58 @@ const extractOperator = (selector: string, context: Cheerio) =>
 
 const extractTransactions = (selector: string, context: Cheerio) =>
     cheerio(selector, context) &&
-    <{}[]>[]; // TODO: Extract Each Transaction row
+    <Maybe<Transaction[]>>{}; // TODO: Extract Each Transaction row
 
-const parseReceiptPage = (page: Page, pom: ReceiptDetailPageObjectModel): Promise<Receipt> =>
-    page.content()
-        .then((html) => cheerio.load(html))
-        .then($ => $(pom.contentContainer))
-        .then((content) =>
-            (<Receipt>{
-                date: extractDate(pom.date, content),
+const mapRawReceiptToReceipt = (input: RawReceipt): Maybe<Receipt> =>
+    input &&
+    input.date.isJust() &&
+    input.receiptInfo &&
+    input.receiptInfo.lane.isJust() &&
+    input.receiptInfo.operator.isJust() &&
+    input.receiptInfo.place.isJust() &&
+    input.transactions.isJust() &&
+    input.totalAmount.isJust() &&
+    input.totalSavings.isJust() &&
+    input.totalTax.isJust()
+        ? Just(
+            <Receipt>{
+                date: input.date.extract(),
                 receiptInfo: {
-                    place: extractPlace(pom.receiptInfo.place, content),
-                    lane: extractLane(pom.receiptInfo.lane, content),
-                    operator: extractOperator(pom.receiptInfo.operator, content),
+                    lane: input.receiptInfo.lane.extract(),
+                    operator: input.receiptInfo.operator.extract(),
+                    place: input.receiptInfo.place.extract(),
                 },
-                transactions: extractTransactions(pom.transactionRow, content),
-                totalSavings: extractFloat(pom.totalAmounts.totalSavings, content),
-                totalTax: extractFloat(pom.totalAmounts.tax, content),
-                totalAmount: extractFloat(pom.totalAmounts.total, content),
+                transactions: input.transactions.extract(),
+                totalAmount: input.totalAmount.extract(),
+                totalSavings: input.totalSavings.extract(),
+                totalTax: input.totalTax.extract(),
             })
-        );
+        : Nothing;
+
+const parseReceiptTransactionsPage = R.curry(
+    (pom: ReceiptDetailPageObjectModel, page: Page): Promise<Maybe<Receipt>> =>
+        page.content()
+            .then((html) => cheerio.load(html))
+            .then($ => $(pom.contentContainer))
+            .then((content) =>
+                (<RawReceipt>{
+                    date: extractDate(pom.date, content),
+                    receiptInfo: {
+                        place: extractPlace(pom.receiptInfo.place, content),
+                        lane: extractLane(pom.receiptInfo.lane, content),
+                        operator: extractOperator(pom.receiptInfo.operator, content),
+                    },
+                    transactions: extractTransactions(pom.transactionRow, content),
+                    totalSavings: extractFloat(pom.totalAmounts.totalSavings, content),
+                    totalTax: extractFloat(pom.totalAmounts.tax, content),
+                    totalAmount: extractFloat(pom.totalAmounts.total, content),
+                })
+            )
+            .then(mapRawReceiptToReceipt)
+);
+
+const navigateToReceiptDetailsPage = (baseUrl: string, detailsUrl: string, page: Page) =>
+    navigateToUrlAndWait(url.resolve(baseUrl, detailsUrl), page);
 
 /**
  * Navigate to a receipt summaries' detail page, and return the parsed Receipt
@@ -64,16 +122,16 @@ const parseReceiptPage = (page: Page, pom: ReceiptDetailPageObjectModel): Promis
  * @param pom
  * @param receiptSummary
  */
-const getReceiptDetails = R.curry(
-    (baseUrl: string,
-    page: Page,
-    pom: ReceiptDetailPageObjectModel,
-    receiptSummary: ReceiptSummary):
-    Promise<Maybe<Receipt>> =>
+const getReceiptTransactions = R.curry(
+    (
+        baseUrl: string,
+        page: Page,
+        pom: ReceiptDetailPageObjectModel,
+        receiptSummary: ReceiptSummary
+    ): Promise<Maybe<Receipt>> =>
 
-        navigateToUrlAndWait(url.resolve(baseUrl, receiptSummary.url), page)
-            .then(() => parseReceiptPage(page, pom))
-            .then((receipt) => Maybe.fromNullable(receipt)),
+        navigateToReceiptDetailsPage(baseUrl, receiptSummary.url, page)
+            .then(parseReceiptTransactionsPage(pom)),
 );
 
 /**
@@ -86,25 +144,21 @@ const getReceiptDetails = R.curry(
  *  @param summaries
  */
 const fetchTransactionsForEachReceipt = R.curry(
-    (receiptTransactionsFetcher: (summaries: ReceiptSummary) => Promise<Maybe<Receipt>>, summaries: ReceiptSummary[]) => {
+    (receiptTransactionsFetcher: (summaries: ReceiptSummary) => Promise<Maybe<Receipt>>, summaries: ReceiptSummary[]) =>
 
-    /// TODO: Really refactor this mapper, this is way too hard to understand, and doesn't work at all
+        /// TODO: Really refactor this mapper, this is way too hard to understand, and doesn't work at all
+        // Get the Transactions for Each Receipt Summary
+        Promise.all(R.map(receiptTransactionsFetcher, summaries))
 
-    const details = R.map(receiptTransactionsFetcher, summaries);
+            // Filter out the transactions that returned as 'Nothing'
+            .then(result => R.filter((maybeReceipt: Maybe<Receipt>) => maybeReceipt.isJust(), result))
 
-    const promises = Promise.all(details);
+            // Get all of the maybes together
+            .then( result => Maybe.catMaybes(result))
 
-    return promises
-        .then(result => {
-            return R.filter((maybeReceipt: Maybe<Receipt>) => maybeReceipt.isJust(), result);
-        })
-        .then( result => {
-            return Maybe.catMaybes(result);
-        })
-        .then( result => {
-            return Maybe.fromNullable(result);
-        });
-});
+            // Re-group the maybes from a possible nullable.
+            .then( result => Maybe.fromNullable(result))
+);
 
 
 
@@ -117,15 +171,19 @@ const getAllReceiptDetails = R.curry(
     ): Promise<ActionResponse<Receipt[]>> =>
 
         receiptSummaries.isNothing()
+
+            // Input was nothing? return Nothing
             ? Promise.resolve( { page, result: Nothing })
 
             // TODO: This might just need to be a small wrapper function that operates on all receiptSummaries
             // EG: R.map(fetchReceiptTransactions, receiptSummaries.extract());
             // Need to handle the promises though....
             : fetchTransactionsForEachReceipt(
-                getReceiptDetails(baseUrl, page, pom),
+                getReceiptTransactions(baseUrl, page, pom),
                 receiptSummaries.extract()
             )
+
+                // Return back the 'Action Result'
                 .then(result => ({
                     page,
                     result,
@@ -140,9 +198,9 @@ export {
     extractPlace,
     extractTransactions,
     getAllReceiptDetails,
-    getReceiptDetails,
+    getReceiptTransactions,
     fetchTransactionsForEachReceipt,
-    parseReceiptPage,
+    parseReceiptTransactionsPage,
 };
 
 export default getAllReceiptDetails;
